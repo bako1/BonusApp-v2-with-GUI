e2349b8f9693b6347f1069e73bd1993312fe75a9
Bako_1 <abdimb@stud.ntnu.no>
2020-03-28 16:20:01 +0100

    clearance

--- MemberBonusApp_GUI/src/sample/BasicMember.java
+++ MemberBonusApp_GUI/src/sample/BasicMember.java
@@ -3,12 +3,13 @@ package sample;
 import java.time.LocalDate;
 
 /**
- * The class inherits the Class BonusMember
+ * The class inherits the Class BonusMember.
  * All members are BasicMembers initially, Based on the earned points they could be
- * upgraded to either silver or Gold member
+ * upgraded to either silver or Gold member.
  * */
-public class BasicMember extends BonusMember{
+public class BasicMember extends BonusMember {
     public BasicMember(int memberNo, Personals personals, LocalDate enrolledDate) {
+
         super(memberNo, personals, enrolledDate);
     }
 }
--- MemberBonusApp_GUI/src/sample/Main.java
+++ MemberBonusApp_GUI/src/sample/Main.java
@@ -19,310 +19,313 @@ import javafx.stage.Stage;
 
 public class Main extends Application  {
   private Controller controller = new Controller();
+  private String buttonsBorderColor = "-fx-border-color: red";
 
 
 
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
 
+  @Override
+  public void start(Stage primaryStage) throws Exception {
 
 
-        /**
-         * Table*/
 
-//table column for first name
-        TableColumn<TableClass, String> firstName = new TableColumn<>("firstName");
-        firstName.setMaxWidth(200);
-        firstName.setCellValueFactory(new PropertyValueFactory<>("firstName"));
+    // table column for first name
 
-        //table column for surname
 
-        TableColumn<TableClass, String> surname = new TableColumn<>("Surname");
-        surname.setMaxWidth(100);
-        surname.setCellValueFactory(new PropertyValueFactory<>("surname"));
-        //table column for Member number
-        TableColumn<TableClass, Integer> memberNo = new TableColumn<>("Member.No");
-        memberNo.setMaxWidth(100);
-        memberNo.setCellValueFactory(new PropertyValueFactory<>("memberNo"));
 
+    TableColumn<TableClass, String> firstName = new TableColumn<>("firstName");
+    firstName.setMaxWidth(200);
+    firstName.setCellValueFactory(new PropertyValueFactory<>("firstName"));
 
-        /**
-         *
-         *
-         *
-         * **/
+    //table column for surname
 
+    TableColumn<TableClass, String> surname = new TableColumn<>("Surname");
+    surname.setMaxWidth(100);
+    surname.setCellValueFactory(new PropertyValueFactory<>("surname"));
+    //table column for Member number
+    TableColumn<TableClass, Integer> memberNo = new TableColumn<>("Member.No");
+    memberNo.setMaxWidth(100);
+    memberNo.setCellValueFactory(new PropertyValueFactory<>("memberNo"));
 
-        Button addNew = new Button("Add");
 
-        Image image = new Image(getClass().getResourceAsStream("addNewMember.png"));
-        ImageView imageView = new ImageView(image);
-        imageView.setFitHeight(25);
-        imageView.setFitWidth(25);
-        addNew.setGraphic(imageView);
-        addNew.setBackground(new Background(new BackgroundFill(Color.GRAY,CornerRadii.EMPTY,Insets.EMPTY)));
-        /***
-         * Add set onAction
-         */
-       addNew.setOnAction(add->
-       controller.onAddClicked());
 
+    //Add button
 
+    Button addNew = new Button("Add");
 
+    Image image = new Image(getClass().getResourceAsStream("addNewMember.png"));
+    ImageView imageView = new ImageView(image);
+    imageView.setFitHeight(25);
+    imageView.setFitWidth(25);
+    addNew.setGraphic(imageView);
+    addNew.setBackground(new Background(new
+        BackgroundFill(Color.GRAY,CornerRadii.EMPTY,Insets.EMPTY)));
+    addNew.setOnAction(add -> controller.onAddClicked());
 
 
-        Button deleteM = new Button("Remove");
-        Button registerPoints = new Button("Register \nPoints");
-        Button showDetails  = new Button("Details");
 
-        Image image2 = new Image(getClass().getResourceAsStream("removeMember.png"));
-        ImageView image2View = new ImageView(image2);
-        image2View.setFitHeight(25);
-        image2View.setFitWidth(25);
-        deleteM.setGraphic(image2View);
+    //Delete Button
 
+    Button deleteM = new Button("Remove");
+    deleteM.setBackground(new Background(new
+        BackgroundFill(Color.GRAY,CornerRadii.EMPTY,Insets.EMPTY)));
+    deleteM.setPrefSize(100,70);
+    deleteM.setStyle(buttonsBorderColor);
+    //Delete set on action
+    deleteM.setOnAction(del -> controller.onDeleteClicked());
 
-        deleteM.setBackground(new Background(new BackgroundFill(Color.GRAY,CornerRadii.EMPTY,Insets.EMPTY)));
 
-        deleteM.setOnAction(del->controller.onDeleteClicked());
 
-        Image image3 = new Image(getClass().getResourceAsStream("viewDetails.png"));
-        ImageView image3View = new ImageView(image3);
-        image3View.setFitHeight(25);
-        image3View.setFitWidth(25);
-        showDetails.setGraphic(image3View);
 
 
-        showDetails.setBackground(new Background(new BackgroundFill(Color.GRAY,CornerRadii.EMPTY,Insets.EMPTY)));
+    Button showDetails  = new Button("Details");
 
-        registerPoints.setBackground(new Background(new BackgroundFill(Color.GRAY,CornerRadii.EMPTY,Insets.EMPTY)));
 
-        showDetails.setOnAction(show->controller.onSeeDetail());
-        String borderColor = "-fx-border-color: red";
+    showDetails.setBackground(new Background(new
+        BackgroundFill(Color.GRAY,CornerRadii.EMPTY,Insets.EMPTY)));
 
-        addNew.setPrefSize(100,70);
-        addNew.setStyle(borderColor);
-        deleteM.setPrefSize(100,70);
-        deleteM.setStyle(borderColor);
 
-        showDetails.setPrefSize(100,70);
-        showDetails.setStyle(borderColor);
+    showDetails.setOnAction(show -> controller.onSeeDetail());
 
-        registerPoints.setPrefSize(100,70);
-        registerPoints.setStyle(borderColor);
+    Image image2 = new Image(getClass().getResourceAsStream("removeMember.png"));
+    ImageView image2View = new ImageView(image2);
+    image2View.setFitHeight(25);
+    image2View.setFitWidth(25);
+    deleteM.setGraphic(image2View);
 
 
-        Image image4 = new Image(getClass().getResourceAsStream("registerP.png"));
-        ImageView image4View = new ImageView(image4);
-        image4View.setFitHeight(25);
-        image4View.setFitWidth(25);
-        registerPoints.setGraphic(image4View);
 
-        HBox adddshdel = new HBox(10);
 
+    Image image3 = new Image(getClass().getResourceAsStream("viewDetails.png"));
+    ImageView image3View = new ImageView(image3);
+    image3View.setFitHeight(25);
+    image3View.setFitWidth(25);
+    showDetails.setGraphic(image3View);
 
-        registerPoints.setOnAction(rP->controller.onRegisterClicked());
 
-        adddshdel.setOpaqueInsets(new Insets(10,10,10,10));
-        adddshdel.setBackground( new Background(new BackgroundFill(Color.PALEGOLDENROD,CornerRadii.EMPTY,Insets.EMPTY)));
-        adddshdel.getChildren().addAll(addNew,deleteM,showDetails,registerPoints);
 
-        /**
-         * Table view
-         *
-         *
-         */
+    addNew.setPrefSize(100,70);
+    addNew.setStyle(buttonsBorderColor);
 
-        controller.tableView.getOnScroll();
-        controller.tableView.getColumns().addAll(surname, firstName,memberNo);
-        controller.tableView.setItems(controller.initiate());
-        controller.tableView.setTableMenuButtonVisible(true);
 
+    showDetails.setPrefSize(100,70);
+    showDetails.setStyle(buttonsBorderColor);
 
-        String styles =
+    //Register Button
+    Button registerPoints = new Button("Register \nPoints");
+    registerPoints.setBackground(new Background(new
+        BackgroundFill(Color.GRAY,CornerRadii.EMPTY,Insets.EMPTY)));
 
-              "-fx-border-color:  yellow" ;
 
-        controller.tableView.setStyle(styles);
+    registerPoints.setPrefSize(100,70);
+    registerPoints.setStyle(buttonsBorderColor);
+    //Setting register button icon
 
+    Image image4 = new Image(getClass().getResourceAsStream("registerP.png"));
+    ImageView image4View = new ImageView(image4);
+    image4View.setFitHeight(25);
+    image4View.setFitWidth(25);
+    registerPoints.setGraphic(image4View);
 
+    //register set onAction
+    registerPoints.setOnAction(registerP -> controller.onRegisterClicked());
 
-        Label label = new Label("Member List");
-        label.setFont(Font.font("verdana", FontWeight.BOLD, 30));
 
+    HBox adddshdel = new HBox(10);
 
-        label.setMinWidth(300);
-        HBox labelBox = new HBox(10);
-        labelBox.getChildren().add(label);
-        labelBox.setOpaqueInsets(new Insets(10, 10, 10, 10));
-        labelBox.setFillHeight(true);
 
 
-        VBox root = new VBox();
-        root.setStyle(styles);
-        root.setBackground(new Background(new BackgroundFill(Color.BISQUE, CornerRadii.EMPTY, Insets.EMPTY)));
-        controller.tableView.setBackground(new Background(new BackgroundFill[100]));
+    adddshdel.setOpaqueInsets(new Insets(10,10,10,10));
+    adddshdel.setBackground(new Background(new
+        BackgroundFill(Color.PALEGOLDENROD,CornerRadii.EMPTY,Insets.EMPTY)));
+    adddshdel.getChildren().addAll(addNew,deleteM,showDetails,registerPoints);
 
-VBox tableBox=new VBox(10);
+    /*
+     * Table view
 
-tableBox.setPadding(new Insets(50,10,10,10));
+     */
 
-tableBox.getChildren().addAll(adddshdel);
+    controller.tableView.getOnScroll();
+    controller.tableView.getColumns().addAll(surname, firstName,memberNo);
+    controller.tableView.setItems(controller.initiate());
+    controller.tableView.setTableMenuButtonVisible(true);
 
-/**
- * Text area where Detail info about selected members display
- * */
-        Label basicLabel = new Label("Basic Members");
-      controller.basicMember.setStyle("-fx-border-color: white");
 
+    String styles = "-fx-border-color:  yellow";
 
-        Label silverLabel = new Label("Silver Members");
-        controller.silverMember.setStyle("-fx-background-color: silver");
-        Label goldLabel = new Label("Gold Members");
-        controller.goldMember.setStyle("-fx-background-color: gold");
+    controller.tableView.setStyle(styles);
 
 
-        root.getChildren().addAll(tableBox,controller.tableView, basicLabel,controller.basicMember,silverLabel,
-              controller.silverMember,goldLabel,controller.goldMember); //Add tableView to VBox
 
-        /**
-         *
-         * Delete Button
-         * */
+    Label label = new Label("Member List");
+    label.setFont(Font.font("verdana", FontWeight.BOLD, 30));
 
-        //Delete Button
-        Button delete = new Button("Delete");
-        //set on action
-        delete.setOnAction(d ->
 
-           controller.onDeleteClicked());
+    label.setMinWidth(300);
+    HBox labelBox = new HBox(10);
+    labelBox.getChildren().add(label);
+    labelBox.setOpaqueInsets(new Insets(10, 10, 10, 10));
+    labelBox.setFillHeight(true);
 
-        HBox hBox = new HBox();
-/***
- * MENU
- * */
 
+    VBox root = new VBox();
+    root.setStyle(styles);
+    root.setBackground(new Background(new
+        BackgroundFill(Color.BISQUE, CornerRadii.EMPTY, Insets.EMPTY)));
+    controller.tableView.setBackground(new Background(new BackgroundFill[100]));
 
-        MenuItem seeDetail;
-        MenuItem deleteMember;
-        MenuItem registerPoint;
-        ContextMenu contextMenu;
+    VBox tableBox = new VBox(10);
 
+    tableBox.setPadding(new Insets(50,10,10,10));
 
-        seeDetail = new MenuItem("See Detail");
+    tableBox.getChildren().addAll(adddshdel);
 
-        deleteMember = new MenuItem(" Remove");
-        registerPoint = new MenuItem("Register Point");
-        contextMenu = new ContextMenu();
 
-        contextMenu.getItems().addAll(seeDetail, deleteMember, registerPoint);
+    //Text area where Detail info about selected members display
 
+    controller.basicMember.setStyle("-fx-border-color: white");
 
-        surname.setContextMenu(contextMenu);
+    Label silverLabel = new Label("Silver Members");
+    controller.silverMember.setStyle("-fx-background-color: silver");
+    Label goldLabel = new Label("Gold Members");
+    controller.goldMember.setStyle("-fx-background-color: gold");
+    Label basicLabel = new Label("Basic Members");
 
-        controller.tableView.setOnMouseClicked(click -> {
-            if (click.getButton() == MouseButton.SECONDARY) {
-                TableClass selectedItem = controller.tableView.getSelectionModel().getSelectedItem();
-                try {
 
-                    if (!selectedItem.surname.equals("")) {
 
-                        contextMenu.show(controller.tableView, click.getSceneX(), click.getSceneY());
-                        deleteMember.setOnAction(remove ->controller.onDeleteClicked());
-                        registerPoint.setOnAction(reg ->controller.onRegisterClicked());
-                    seeDetail.setOnAction(see->controller.onSeeDetail());
+    root.getChildren().addAll(tableBox,controller.tableView,
+        basicLabel,controller.basicMember,silverLabel,
+        controller.silverMember,goldLabel,controller.goldMember); //Add tableView to VBox
 
 
-                    }
 
-                } catch (NullPointerException e) {
-                  System.out.println(e.getMessage());
-                }
-            }
-        });
+    //Delete Button
+    Button delete = new Button("Delete");
+    //set on action
+    delete.setOnAction(d -> controller.onDeleteClicked());
 
+    // MENU
 
 
-        /**
-         * set Up stage and scene
-         *
-         * */
 
-        root.setSpacing(5);
-        root.setPadding(new Insets(10, 10, 10, 10));
-        root.getChildren().addAll(hBox);
+    MenuItem seeDetail;
+    MenuItem deleteMember;
+    MenuItem registerPoint;
 
-        primaryStage.setTitle("AirLine Bonus App");
-        primaryStage.setScene(new Scene(root, 700, 700));
-        primaryStage.show();
 
+    seeDetail = new MenuItem("See Detail");
 
-        /*********
-         *
-         * Login page
-         *
-         *
-         */
+    deleteMember = new MenuItem(" Remove");
+    registerPoint = new MenuItem("Register Point");
+    ContextMenu contextMenu;
 
+    contextMenu = new ContextMenu();
 
-        Label usernameL = new Label("Username:  ");
-        Label passL = new Label("Password:  ");
-        Button log =new Button("   Login ");
-        log.setMaxSize(700,50);
+    contextMenu.getItems().addAll(seeDetail, deleteMember, registerPoint);
 
 
-        TextField username = new TextField();
+    surname.setContextMenu(contextMenu);
 
-        username.setPromptText("Username");
+    controller.tableView.setOnMouseClicked(click -> {
+      if (click.getButton() == MouseButton.SECONDARY) {
+        TableClass selectedItem = controller.tableView.getSelectionModel().getSelectedItem();
+        try {
 
-        PasswordField pass = new PasswordField();
+          if (!selectedItem.surname.equals("")) {
 
+            contextMenu.show(controller.tableView,
+                click.getSceneX(), click.getSceneY());
+            deleteMember.setOnAction(remove -> controller.onDeleteClicked());
+            registerPoint.setOnAction(reg -> controller.onRegisterClicked());
+            seeDetail.setOnAction(see -> controller.onSeeDetail());
 
 
-        pass.setPromptText("********");
-        GridPane gridPane = new GridPane();
+          }
 
-        gridPane.add(usernameL,0,0);
-        gridPane.add(username,1,0);
+        } catch (NullPointerException e) {
+          System.out.println(e.getMessage());
+        }
+      }
+    });
 
 
-        gridPane.add(passL,0,4);
 
-        gridPane.add(pass,1,4);
-        gridPane.add(log,1,5);
 
+    // set Up stage and scene
 
 
-        gridPane.setVgap(10);
-        HBox rootLog = new HBox(10);
-        rootLog.getChildren().addAll(gridPane);
-       rootLog.setBackground(new Background(new BackgroundFill(Color.PALEGOLDENROD,CornerRadii.EMPTY,Insets.EMPTY)));
+    root.setSpacing(5);
+    root.setPadding(new Insets(10, 10, 10, 10));
+
+    HBox hBox1 = new HBox();
+    root.getChildren().addAll(hBox1);
+
+    primaryStage.setTitle("AirLine Bonus App");
+    primaryStage.setScene(new Scene(root, 700, 700));
+    primaryStage.show();
+
+
+    //login page
+
+
+    Label usernameL = new Label("Username:  ");
+    usernameL.setFont(Font.font("Arial",20));
+
+    Label passL = new Label("Password:  ");
+    passL.setFont(Font.font("Times New Roman",25));
+    Button log = new Button("   Login ");
+    log.setMaxSize(700,50);
+
+
+    TextField username = new TextField();
+
+    username.setPromptText("Username");
+
+    PasswordField pass = new PasswordField();
+
+
+
+    pass.setPromptText("********");
+    GridPane gridPane = new GridPane();
+
+    gridPane.add(usernameL,0,0);
+    gridPane.add(username,1,0);
+
+
+    gridPane.add(passL,0,4);
+
+    gridPane.add(pass,1,4);
+    gridPane.add(log,1,5);
+
+
+
+    gridPane.setVgap(10);
+    HBox rootLog = new HBox(10);
+    rootLog.getChildren().addAll(gridPane);
+    rootLog.setBackground(new Background(new BackgroundFill(
+        Color.PALEGOLDENROD,CornerRadii.EMPTY,Insets.EMPTY)));
     rootLog.setPadding(new Insets(100,10,10,10));
 
     rootLog.setAlignment(Pos.CENTER);
 
-        Scene scene = new Scene(rootLog,500,500);
-        Stage stageLog = new Stage();
-        stageLog.setTitle("Login page");
+    Scene scene = new Scene(rootLog,500,500);
+    Stage stageLog = new Stage();
+    stageLog.setTitle("Login page");
 
-        stageLog.setScene(scene);
-        rootLog.setBackground(new Background(new BackgroundFill(Color.AZURE,CornerRadii.EMPTY,Insets.EMPTY
-        )));
+    stageLog.setScene(scene);
+    rootLog.setBackground(new Background(new
+        BackgroundFill(Color.AZURE,CornerRadii.EMPTY,Insets.EMPTY
+    )));
 
 
-        primaryStage.close();
-log.setOnAction(e->{
-
-    if(username.getText().equals("bako") && pass.getText().equals("45548979")){
-    primaryStage.show();
-    stageLog.close();
-
-    usernameL.setFont(Font.font("Arial",20));
-        passL.setFont(Font.font("Times New Roman",25));
-
-    }else {
-
+    primaryStage.close();
+    log.setOnAction(e -> {
+      //Not practical i used it only to show the login page
+      // which requires both username and  password
+      // password and username should have been securely saved in database.
+      if (!username.getText().equals("bako")
+          || !pass.getText().equals("45548979")) {
         pass.setText("password");
 
         Text message = new Text();
@@ -335,27 +338,30 @@ log.setOnAction(e->{
         message.setFont(Font.font("Times New Roman",20));
         gridPane.add(message,1,6);
         message.setLineSpacing(10);
+      } else {
+        primaryStage.show();
+        stageLog.close();
+      }
 
-    }
-});
-        stageLog.show();
-
-
-
-
-
+    });
+    stageLog.show();
 
 
 
 
 
 
-    }
 
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+
+
+
+  }
+
+
+  public static void main(String[] args) {
+    launch(args);
+  }
 
 
 
